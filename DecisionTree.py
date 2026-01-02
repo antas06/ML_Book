8e5cf50e23d459c54857ee6b2d5ec877fa7d179e
antas06 <antas.hft@gmail.com>
2026-01-03 01:17:45 +0530

    Added Node Class and inited DecisionTree class

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,2 +1,24 @@
 import numpy as np
 
+class Node:
+  def __init__(self, feature =None, threshold = None, left =None, right = None, *, value = None ):
+    self.value = value
+    self.left= left
+    self.right= right
+    self.feature= feature
+    self.threshold= threshold
+  
+  def is_leaf_node(self):
+    return True if self.value else False
+
+
+class Decisiontree:
+  def __init__(self, min_smaple_split=2, max_depth = 100, n_features = None):
+    self.min_sample_split = min_smaple_split
+    self.max_depth = max_depth
+    self.n_features = n_features
+    self.root = None
+
+  
+
+  
