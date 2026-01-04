5e2623e42ece5e66b88053b5517ccfefa64f4dab
antas06 <antas.hft@gmail.com>
2026-01-04 18:20:21 +0530

    Cleaned the code and added fit()

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,24 +1,30 @@
 import numpy as np
 
+#Creating a class Node
 class Node:
-  def __init__(self, feature =None, threshold = None, left =None, right = None, *, value = None ):
+  def __init__(self, feature=None, threshold=None, left=None, right=None, value=None):
+    self.feature = feature
+    self.threshold = threshold
+    self.left = left
+    self.right = right
     self.value = value
-    self.left= left
-    self.right= right
-    self.feature= feature
-    self.threshold= threshold
-  
-  def is_leaf_node(self):
-    return True if self.value else False
 
-
-class Decisiontree:
-  def __init__(self, min_smaple_split=2, max_depth = 100, n_features = None):
-    self.min_sample_split = min_smaple_split
+#Class Decision Tree
+class DecisionTree:
+  #init the props of dt.
+  def __init__(self, max_depth=5, n_features=None):
     self.max_depth = max_depth
     self.n_features = n_features
     self.root = None
 
+#Growing tree from root
+  def fit(self, X,y):
+    self.root = self._grow_tree(X,y, depth =0)
+
   
+
+    
+
+
 
   
