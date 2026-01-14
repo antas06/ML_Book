016bb230c38e40a09e1d59e7c3e4758d9394fd75
antas06 <antas.hft@gmail.com>
2026-01-14 22:48:33 +0530

    Updated

--- NavieBayes.py
+++ NavieBayes.py
@@ -1,34 +1,40 @@
 import numpy as np
 
-class GaussianNaiveBayes:
+class NavieBayes:
 
-    def fit(self, X, y):
-        self.classes = np.unique(y)
-        self.mean = {}
-        self.var = {}
-        self.prior = {}
+  def fit(self, X, y):
+    self.n_classes = np.unique(y)
+    self.mean = {}
+    self.var = {}
+    self.c_prob ={}
 
-        for c in self.classes:
-            X_c = X[y == c]
-            self.mean[c] = np.mean(X_c, axis=0)
-            self.var[c] = np.var(X_c, axis=0)
-            self.prior[c] = X_c.shape[0] / X.shape[0]
+    for c in self.n_classes:
+      X_c = X[y==c]
+      self.mean[c] = np.mean(X_c)
+      self.var[c] = np.var(X_c)
+      self.c_prob[c] = X_c.shape[0]/X.shape[0]
 
-    def gaussian(self, x, mean, var):
-        return np.exp(-(x - mean) ** 2 / (2 * var)) / np.sqrt(2 * np.pi * var)
+  # Gaussian Formula, (woh wala curve, chotlundu)
+  def gaussian(self, x, mean, var):
+    return np.exp(-(x - mean) ** 2 / (2 * var)) / np.sqrt(2 * np.pi * var)
+  
+  def predict(self, X):
+    predictions = []
 
-    def predict(self, X):
-        predictions = []
+    for x in X:
+      posteriors = []
 
-        for x in X:
-            posteriors = []
+      for c in self.n_classes:
+        prior = np.log(self.c_prob[c])
+        likelihood = np.sum(np.log(self.gaussian(x, self.mean[c], self.var[c])))
+        posterior = prior + likelihood
+        posteriors.append(posterior)
 
-            for c in self.classes:
-                prior = np.log(self.prior[c])
-                likelihood = np.sum(np.log(self.gaussian(x, self.mean[c], self.var[c])))
-                posterior = prior + likelihood
-                posteriors.append(posterior)
+      predictions.append(self.classes[np.argmax(posteriors)])
 
-            predictions.append(self.classes[np.argmax(posteriors)])
-
-        return np.array(predictions)
+    return np.array(predictions)
+  
+  #score for evaluation
+  def score(self, X, y):
+    predictions = self.predict(X)
+    return np.mean(predictions == y)
